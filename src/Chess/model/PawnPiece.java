27e073e0c4026563e54124f2ce02f813edf806a2
jcherry45 <jcherry45@users.noreply.github.com>
2019-11-15 12:33:43 -0800

    Update PawnPiece.java

--- src/Chess/model/PawnPiece.java
+++ src/Chess/model/PawnPiece.java
@@ -1,9 +1,33 @@
+  
 package Chess.model;
 
 import Chess.model.Piece;
 
 public class PawnPiece  extends Piece {
-    public PawnPiece(String team) {
-        super(PieceType.Pawn, team);
+    String path;
+    private int x = -1;
+    private int y = -1;
+	public PawnPiece(String team) {
+        super("pawn", team);
+        this.setPath(team);
+    }
+	
+    public void setPath(String color) {
+    	if(color.equalsIgnoreCase("white")) {
+    		this.path = "Chess/images/pieces/white_pawn.png";
+    	}
+    		
+    	if(color.equalsIgnoreCase("black")) {
+    		this.path = "Chess/images/pieces/black_pawn.png";
+    	}
+    }
+    
+    public String getPath() {
+    	return this.path;
+    }
+    @Override
+    public boolean isMoveValid(int x, int y, Block blocks[][]){
+    	
+		return true;
     }
 }
