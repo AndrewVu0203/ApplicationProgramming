9db0567b5d88b0d56f0453a902df23563a5b8079
jcherry45 <jcherry45@users.noreply.github.com>
2019-11-15 12:22:27 -0800

    Update Block.java

--- src/Chess/model/Block.java
+++ src/Chess/model/Block.java
@@ -49,3 +49,59 @@ public class Block extends Button {
         this.setGraphic(image);
     }    
 }
+//package Chess.model;
+//
+//import javafx.scene.control.Button;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+//
+//public class Block extends Button {
+//    int x;
+//    int y;
+//    int color;
+//    private Piece piece = null;
+//    
+//    public Block(){
+//    	
+//    }
+//    
+//    public void removeBlock() {
+//    	this.setGraphic(null);
+//    	this.piece = null;
+//    }
+//    public Piece getPiece() {
+//    	return this.piece;
+//    }
+//    
+//    public void setPiece(Piece piece) {
+//    	this.piece = piece;
+//		ImageView image = new ImageView(new Image(piece.getPath()));
+//        this.setGraphic(image);
+//    }
+//    
+//    public Block(Piece piece, int x, int y) {
+//    	this.piece = new Piece();
+//    	this.x = x;
+//    	this.y = y;
+//    }
+//    
+//    public Block(String text){
+//        super(text);
+//    }
+//    
+//
+//    public int getX() {
+//        return x;
+//    }
+//    public void setX(int x) {
+//        this.x = x;
+//    }
+//
+//    public int getY() {
+//        return y;
+//    }
+//
+//    public void setY(int y) {
+//        this.y = y;
+//    }
+//}
