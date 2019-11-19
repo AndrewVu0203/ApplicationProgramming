d7cddb29e02219ccd554455ca70888832c2ef3c0
EST <55035415+EST320@users.noreply.github.com>
2019-11-19 13:11:41 -0600

    KnightPiece ismovevalid

--- src/Chess/model/KnightPiece.java
+++ src/Chess/model/KnightPiece.java
@@ -11,6 +11,27 @@ public class KnightPiece  extends Piece {
     /* This method will validate whether or not a move is legal */
     @Override
     public boolean isMoveValid(int x, int y, Block[][] blocks){
-		return false;
+        int thisX = this.getX();
+        int thisY = this.getY();
+
+        /* If there is currently a */
+        if(blocks[x][y].getPiece() != null) {
+            if(blocks[x][y].getPiece().getTeam().equalsIgnoreCase(super.getTeam())) {
+                System.out.println("False");
+                return false;
+            }
+        }
+
+        // thisX = cur pos
+        // x = move to pos
+        if((thisX - x == -2 && thisY - y == 1) || (thisX - x == 2 && thisY - y == 1) || (thisX - x == -1 &&thisY - y == 2)
+                || (thisX - x == 1 && thisY - y == 2)|| (thisX - x == 2 && thisY - y == -1)
+                || (thisX - x == -2 && thisY - y == -2) || (thisX - x == 1 && thisY - y == -2)
+                || (thisX - x == -1 && thisY - y == -2)) {
+            this.setX(x);
+            this.setY(y);
+            return true;
+        }
+        return false;
     }
 }
