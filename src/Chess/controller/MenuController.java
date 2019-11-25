8adc9d1e9ff92886bdc6d65875a2bf12919da87d
isaacwwkelly <isaacwwkelly@gmail.com>
2019-11-24 18:21:53 -0600

    Re-commiting the stuff I added recently... Please pull this stuff

--- src/Chess/controller/MenuController.java
+++ src/Chess/controller/MenuController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,7 +17,22 @@ import java.util.ResourceBundle;
 public class MenuController implements Initializable {
 
     public void onePlayer(ActionEvent e) throws IOException {
+        Stage secondStage = new Stage();
+        Parent root2;
+        root2 = FXMLLoader.load(getClass().getResource("/Chess/view/ComingSoon.fxml"));
+        Scene scene = new Scene(root2, 300, 200);
+        secondStage.setScene(scene);
+        secondStage.setTitle("Coming Soon!");
+        secondStage.show();
 
+        long mTime = System.currentTimeMillis();
+        long end = mTime + 3000; // 5 seconds
+
+        while (mTime < end)
+        {
+            mTime = System.currentTimeMillis();
+        }
+        secondStage.close();
     }
 
     public void twoPlayer(ActionEvent e) throws IOException {
