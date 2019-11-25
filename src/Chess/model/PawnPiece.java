a6f32a55061d6038e439789ddccb7fe83d6b09d3
jcherry45 <jcherry45@users.noreply.github.com>
2019-11-24 19:51:44 -0800

    Update PawnPiece.java

--- src/Chess/model/PawnPiece.java
+++ src/Chess/model/PawnPiece.java
@@ -11,15 +11,29 @@ public class PawnPiece  extends Piece {
         int thisY = this.getY();
 
         String color = this.getTeam();
+        
+        if(canAttack(blocks, x, y)){
+        	return true;
+        }
+        
+        if(isCollision(y, blocks, color)) {
+        	return false;
+        }
+        
         if (color.equalsIgnoreCase("black")){
             if(thisX - x == 0 && thisY - y == -1){
                 this.setX(x);
                 this.setY(y);
+                if(this.enPassant == true) {
+                	this.setEnPassant(false);
+                    blocks[x][y-2].setEnPasant(true);
+                }
                 return true;
             }
             if(!hasMoved && (thisX - x == 0 && thisY - y == -2)) {
                 this.setX(x);
                 this.setY(y);
+                blocks[x][y-1].setEnPasant(true);
                 return true;
             }
         }
@@ -29,15 +43,66 @@ public class PawnPiece  extends Piece {
                 this.setX(x);
                 this.setY(y);
                 this.hasMoved = true;
+                if(this.enPassant == true) {
+                	this.setEnPassant(false);
+                    blocks[x][y+2].setEnPasant(true);
+                }
                 return true;
             }
             if(!hasMoved && (thisX - x == 0 && thisY - y == 2)) {
                 this.setX(x);
                 this.setY(y);
+                blocks[x][y+1].setEnPasant(true);
                 this.hasMoved = true;
                 return true;
             }
         }
         return false;
+    }
+    
+    public boolean isCollision(int y, Block[][] blocks, String color) {
+    	int dist = Math.abs(this.getY() - y);
+    	Piece piece;
+    	Block block;
+
+	        if (color.equalsIgnoreCase("black")) {
+	        	for(int i = 1; i <= dist; i++) {
+		    		piece = blocks[this.getX()][this.getY() + i].getPiece();  
+		    		if(piece != null) {
+		    			return true;
+		    		}
+	        	}
+	        }
+	        if (color.equalsIgnoreCase("white")) {
+        		for(int i = 1; i <= dist; i++) {
+		    		block = blocks[this.getX()][this.getY() - i]; 
+		    		piece = block.getPiece();
+		    		if(piece != null) {
+		    			return true;
+		    		}
+        		}
+	        }
+    	return false;
+    }
+    
+    public boolean canAttack(Block[][] blocks, int x, int y) {
+    	Piece piece;
+    	
+    	if(Math.abs(this.getX()-x) == 1 && Math.abs(this.getY() - y) == 1) {
+
+    		piece = blocks[x][y].getPiece();  
+    		if(piece != null) {
+    			this.setX(x);
+                this.setY(y);
+    			return true;
+    		}
+    		if(blocks[x][y].getEnPasant()) {
+
+                blocks[x][y].setEnPasant(false);
+    			return true;
+    		}
+    	}
+    	
+    	return false;
     }
 }
