910171b1288d171a973600bdcb5dcfdf466dfea8
jcherry45 <jcherry45@users.noreply.github.com>
2019-11-29 00:34:14 -0800

    Finished rook piece

--- src/Chess/model/RookPiece.java
+++ src/Chess/model/RookPiece.java
@@ -7,21 +7,159 @@ public class RookPiece extends Piece {
         super(PieceType.Rook, team, x, y);
     }
 
+	
+	
     /* This method will validate whether or not a move is legal */
     @Override
     public boolean isMoveValid(int x, int y, Block[][] blocks){
         int thisX = this.getX();
         int thisY = this.getY();
-
+        
+        
+        if(isCollision(x, y, blocks)) {
+    		return false;
+    	}
+        
+        
         if((Math.abs(thisX - x) < 8 && thisY - y == 0) || (thisX - x == 0 && Math.abs(thisX - y) < 8)) {
-            //cant jump
-//            if(){
-//                return false;
-//            }
+            
             this.setX(x);
             this.setY(y);
             return true;
         }
         return false;
     }
+    
+    public boolean isCollision(int x, int y, Block[][] blocks) {
+    	int i = 0;
+    	Piece piece;
+    	
+    	if(this.getTeam().equalsIgnoreCase("white")) {
+    		
+    		if(this.getX() == x) {
+    			
+    			if(this.getY() > y) {
+        			i = this.getY() - 1;
+    			}
+    			
+    			else {
+        			i = this.getY() + 1;
+    			}
+    			
+    			System.out.printf("%d %d\n", i, this.getX());
+	    		
+    			for(; i != y;) {
+	    			piece = blocks[x][i].getPiece();
+	    			if(piece != null) {
+	    				return true;
+	    			}
+	    			
+	    			if(this.getY() > y) {
+	    				i--;
+	    			}
+	    			
+	    			else {
+	    				i++;
+	    			}
+	    		}
+	    		
+    			this.setX(x);
+	    		this.setY(y);
+	    	}
+	    	
+    		else if(this.getY() == y) {
+    			
+    			if(this.getX() > x) {
+        			i = this.getX() - 1;
+    			}
+    			
+    			else {
+        			i = this.getX() + 1;
+    			}
+    			
+    			for(;i != x;) {
+	    			piece = blocks[i][y].getPiece();
+	    			if(piece != null) {
+	    				return true;
+	    			}
+	    			
+	    			if(this.getX() > x) {
+	    				i--;
+	    			}
+	    			
+	    			else {
+	    				i++;
+	    			}
+	    		}
+	    		
+    			
+	    		
+    			this.setX(x);
+	    		this.setY(y);
+	    	}
+    	}
+    	else if(this.getTeam().equalsIgnoreCase("black")) {
+	    	if(this.getX() == x) {
+	    		
+	    		if(this.getY() > y) {
+        			i = this.getY() - 1;
+    			}
+    			
+    			else {
+        			i = this.getY() + 1;
+    			}
+    			
+	    		for(; i != y;) {
+	    			piece = blocks[x][i].getPiece();
+	    			if(piece != null) {
+	    				return true;
+	    			}
+	    			
+	    			if(this.getY() > y) {
+	    				i--;
+	    			}
+	    			
+	    			else {
+	    				i++;
+	    			}
+	    		}
+	    		
+    			this.setX(x);
+	    		this.setY(y);
+	    	}
+	    	else if(this.getY() == y) {
+	    		
+	    		if(this.getX() > y) {
+        			i = this.getX() - 1;
+    			}
+    			
+    			else {
+        			i = this.getX() + 1;
+    			}
+    			
+	    		for(;i != x;) {
+	    			piece = blocks[i][y].getPiece();
+	    			
+	    			if(piece != null) {
+	    				return true;
+	    			}
+	    			
+	    			if(this.getX() > x) {
+	    				i--;
+	    			}
+	    			
+	    			else {
+	    				i++;
+	    			}
+	    		}
+	    		
+	    		this.setX(x);
+	    		this.setY(y);
+	    	}
+    	}
+    	
+    	
+    	return false;
+    }
+    
 }
